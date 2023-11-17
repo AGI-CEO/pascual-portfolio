dc2daa46adec5021657b758b73f6995cd42477e9
Blaise <jetblaise@gmail.com>
2023-11-17 07:52:32 -0500

    building navbar component

---
+++ src/app/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div className="flex bg-slate-800 p-5 space-x-3">NavBar</div>;
+};
+
+export default NavBar;
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import NavBar from "./NavBar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar />
+      Hello world
+    </main>
+  );
+}
