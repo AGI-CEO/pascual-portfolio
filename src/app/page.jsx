a607a53f4bece9dd1ba14f0566364011b908dbcd
Blaise <jetblaise@gmail.com>
2023-11-17 10:19:06 -0500

    dev

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import NavBar from "./NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavBar />
-      Hello world
+      <p>Hello world</p>
     </main>
   );
 }
