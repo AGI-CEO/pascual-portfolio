80279035b07eba76ad5969c38939b1fa8e5173f6
Blaise <jetblaise@gmail.com>
2023-11-17 22:48:37 -0500

    push code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,29 @@
 "use client";
 import Image from "next/image";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loading from "./loading";
+import Alien from "./models/Alien";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
-      ></Canvas>
+      >
+        <Suspense fallback={Loading}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} />
+          <spotLight position={[15, 20, 5]} angle={0.3} />
+          <hemisphereLight
+            skyColor="#ffffff"
+            groundColor="#000000"
+            intensity={1.0}
+          />
+          <Alien />
+        </Suspense>
+      </Canvas>
       <p>still here</p>
     </main>
   );
