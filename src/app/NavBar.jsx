a4fe8a3f69c8a5ca919d61c2f30add6802383e97
Blaise <jetblaise@gmail.com>
2023-11-17 19:20:28 -0500

    NavBar progress

--- src/app/NavBar.jsx
+++ src/app/NavBar.jsx
@@ -1,7 +1,39 @@
+"use client";
+
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <div className="flex bg-slate-800 p-5 space-x-3">NavBar</div>;
+  return (
+    <header className="header">
+      <nav className="p-1 m-1">
+        <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-row justify-start items-center w-80">
+            <Link
+              href="/"
+              className="w-20 h-20 rounded-lg bg-white items-center justify-center flex flex-row font-bold shadow-md"
+              passHref
+            >
+              <p className="bg-gradient-to-br from-yellow-300 to-pink-500 text-transparent  bg-clip-text items-center justify-center flex flex-row text-lg text-center">
+                Blaise Pascual
+              </p>
+            </Link>
+          </div>
+          <div className="flex flex-row gap-7">
+            <Link href="/about" passHref>
+              <p>About Me</p>
+            </Link>
+            <Link href="/resume" passHref>
+              <button type="button">Resume</button>
+            </Link>
+            <Link href="/projects" passHref>
+              <button type="button">Projects</button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,15 @@
+"use client";
 import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello world</p>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      ></Canvas>
+      <p>still here</p>
     </main>
   );
 }
