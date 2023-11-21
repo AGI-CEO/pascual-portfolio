ae4e272331d487db1eb2e85852f07074b43002e6
Blaise <jetblaise@gmail.com>
2023-11-21 06:35:18 -0500

    Refactor skills table and add pagination

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
-I'm not here to play.
+# My Portfolio
 
-I'm here to take the fuck over.
+Welcome to my portfolio! This project is a showcase of my technical skills and expertise, built over the course of 2 working days. It's a full-stack project, demonstrating my proficiency in a variety of technologies and tools.
 
-All of it is mine.
+## Technologies Used
+
+This project was built using a combination of powerful technologies:
+
+- **Three.js**: A cross-browser JavaScript library used to create and display animated 3D computer graphics in a web browser.
+- **ReactJS**: A JavaScript library for building user interfaces, allowing me to create reusable UI components.
+- **NextJS**: A React framework that enables features like server-side rendering and generating static websites for React based web applications.
+- **Tailwind CSS**: A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.
+- **Daisy UI**: A plugin for Tailwind CSS that adds new utilities, components and a theme builder.
+- **Supabase**: An open source Firebase alternative, used for building the backend of the project. It provides instant APIs for real-time applications with built-in support for user management and third-party login.
+
+## Development and Deployment
+
+The project was developed using the **Bun.js** runtime, a minimal, fast JavaScript runtime with a built-in package manager. It was then deployed to **Azure** using **npm**, a package manager for the JavaScript programming language.
+
+## Technical Skills Demonstrated
+
+This project demonstrates my expertise in the following areas:
+
+- **Next.js**: I've used Next.js to build a server-rendered React application with ease. It's a powerful framework that simplifies the setup of a server-rendered React app.
+- **Routing**: I've built custom routes using Supabase, demonstrating my understanding of how to structure an application's navigation.
+- **Full-Stack Development**: This project is a full-stack application, meaning I've worked on both the front-end and back-end. This includes designing the UI, creating the API, and setting up the database.
+- **Three.js**: I've utilized Three.js to create interactive 3D graphics for the web. This library has allowed me to integrate complex 3D elements into the portfolio, enhancing the user experience and demonstrating my ability to work with advanced JavaScript libraries.
+
+## Future Plans
+
+- **TODO**: In the future, I plan to integrate the `ai.jsx` library to add AI/ML functionality to the app.
+- **TODO**: I also intend to demonstrate my skills in data visualization and deep learning in future projects.
+- **TODO**: I am planning to enhance the 3D models and animations in the portfolio. I am actively working on improving my skills in this area.
+
+## Getting Started
+
+To start working on this project, follow these steps:
+
+1. **Fork the repository**: Click on the 'Fork' button at the top right corner of this repository. This creates a copy of the repository in your GitHub account.
+
+2. **Clone the repository**: Now, clone the forked repository to your local machine. Go to your GitHub account, open the forked repository, click on the 'Code' button and then click the 'copy to clipboard' icon to get the URL. Open a terminal and run the following git command:
+
+```bash
+git clone "url you just copied"
+```
+
+3. **Install the dependencies**: Navigate to the project directory and install the necessary dependencies using Bun.js. Run the following command:
+
+```bash
+bun install
+```
+
+4. **Run the project**: Finally, you can start the project by running the following command:
+
+```bash
+bun run dev
+```
+
+Now, you should be able to see the project running on localhost.
+
+For the implementation of Three.js in this project, I loosely followed this tutorial: [Three.js Tutorial](https://www.youtube.com/watch?v=FkowOdMjvYo&t). However, the entirety of the code was written by me, using the tutorial only as a reference for learning Three.js and as an introduction to building 3d web applications.
+
+## Thank you
+
+I hope you enjoy exploring my portfolio!
--- src/app/about/Skills.jsx
+++ src/app/about/Skills.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Pagination from "react-js-pagination";
 
 const Skills = () => {
   const [skills, setSkills] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     axios
@@ -14,75 +18,48 @@ const Skills = () => {
         console.error(`Error fetching data: ${error}`);
       });
   }, []);
-  const [filter, setFilter] = useState("");
 
   const filteredSkills = skills.filter((skill) => skill.type.includes(filter));
+  const totalPages = Math.ceil(filteredSkills.length / itemsPerPage);
+
+  const skillsOnCurrentPage = filteredSkills.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
-      <div className="relative inline-block w-full">
-        <label
-          htmlFor="filter"
-          className="block text-gray-300 text-sm text-center font-bold mb-2"
-        >
-          Filter by skill type:
-        </label>
-        <div className="relative">
-          <select
-            id="filter"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value)}
-            className="block appearance-none w-full bg-slate-500 border border-gray-400 hover:border-gray-500 px-4 mb-5 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+      {/* ... */}
+      <tbody className="">
+        {skillsOnCurrentPage.map((skill) => (
+          <tr
+            key={skill.name}
+            className="transition-all duration-500 ease-in-out transform hover:scale-105"
           >
-            <option value="">All</option>
-            {Array.from(new Set(skills.map((skill) => skill.type))).map(
-              (type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              )
-            )}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 12l-6-6h12l-6 6z" />
-            </svg>
-          </div>
-        </div>
-      </div>
-      <div className="flex item-center text-center align-middle justify-center">
-        <table>
-          <thead className="">
-            <tr>
-              <th className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-                Skill Name
-              </th>
-              <th className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-                Skill Type
-              </th>
-            </tr>
-          </thead>
-          <tbody className="">
-            {filteredSkills.map((skill) => (
-              <tr
-                key={skill.name}
-                className="transition-all duration-500 ease-in-out transform hover:scale-105"
-              >
-                <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-                  {skill.name}
-                </td>
-                <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-                  {skill.type}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
+              {skill.name}
+            </td>
+            <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
+              {skill.type}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      {/* ... */}
+      <Pagination
+        activePage={currentPage}
+        itemsCountPerPage={itemsPerPage}
+        totalItemsCount={filteredSkills.length}
+        pageRangeDisplayed={5}
+        onChange={handlePageChange}
+        innerClass="flex space-x-2 text-center item-center justify-center mt-4"
+        itemClass="page-item"
+        linkClass="page-link"
+      />
     </>
   );
 };
