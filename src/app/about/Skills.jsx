31af86527a544dfdc5f91424cc046b8e9e845c94
Blaise <jetblaise@gmail.com>
2023-11-29 07:29:15 -0500

    Update Skills component and About page layout

--- src/app/about/Skills.jsx
+++ src/app/about/Skills.jsx
@@ -31,25 +31,49 @@ const Skills = () => {
     setCurrentPage(page);
   };
 
+  const skillTypes = [...new Set(filteredSkills.map((skill) => skill.type))];
+
   return (
     <>
+      {/* Filter dropdown */}
+      <div className="flex dropdown mx-auto justify-center">
+        <div tabIndex={0} role="button" className="btn m-1">
+          {filter ? filter : "Filter by skill type"}
+        </div>
+        <ul className="dropdown-content z-[1] menu  m-5 shadow bg-base-100 rounded-box  grid grid-cols-2  mx-1 px-1">
+          <li>
+            <a role="button" onClick={() => setFilter("")}>
+              Filter by skill type
+            </a>
+          </li>
+          {skillTypes.map((type) => (
+            <li key={type}>
+              <a role="button" onClick={() => setFilter(type)}>
+                {type}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
       {/* ... */}
-      <tbody className="">
-        {skillsOnCurrentPage.map((skill) => (
-          <tr
-            key={skill.name}
-            className="transition-all duration-500 ease-in-out transform hover:scale-105"
-          >
-            <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-              {skill.name}
-            </td>
-            <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
-              {skill.type}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-      {/* ... */}
+      <table className="table table-auto  m-auto text-center">
+        <tbody className=" justify-center text-center">
+          {skillsOnCurrentPage.map((skill) => (
+            <tr
+              key={skill.name}
+              className="table-row transition-all duration-500 ease-in-out transform hover:scale-105"
+            >
+              <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
+                {skill.name}
+              </td>
+              <td className="px-4 py-2 border border-gray-300 bg-white text-gray-600">
+                {skill.type}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        {/* ... */}
+      </table>
       <Pagination
         activePage={currentPage}
         itemsCountPerPage={itemsPerPage}
